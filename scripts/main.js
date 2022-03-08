5ecd2a8c228714091ff074d25a80f4bd76dd3fcf
KingCobra29A <brunerk@umich.edu>
2022-03-08 17:55:26 -0500

    Add createSquare() and CreateRow() functions. Add test function to create 16x16 etch-a-sketch grid. Add classes for grid-squares and grid-rows.

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+
+function createRow(gridLength){
+    const newRow = document.createElement("div");
+    newRow.classList.add("grid-row");
+
+    for (let i = 0; i < gridLength; i++){
+        newRow.appendChild(createSquare());
+    }
+
+    return newRow;
+}
+
+function createSquare(){
+    const newSquare = document.createElement("div");
+    newSquare.classList.add("grid-square", "unselected-square");
+    return newSquare;
+}
+
+function test(rows){
+    const etchGrid = document.querySelector(".body-wrapper");
+    for(let i = 0; i < rows; i++){
+        etchGrid.appendChild(createRow(rows));
+    }    
+}
+
+test(16);
